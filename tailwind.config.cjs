29d897fa8b9e4b45e982a88d30f8d2fb456942ed
Andershaakonsen <64076839+Andershaakonsen@users.noreply.github.com>
2022-11-05 14:06:14 +0100

    init commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require("radix-colors-for-tailwind")({
+      colors: ["mauve", "violet", "green", "red"],
+    }),
+  ],
+};
